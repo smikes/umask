c619971253c3cfc97a8d876575dd1a97b8210897
smikes <smikes@cubane.com>
2015-01-14 20:02:47 -0700

    initial rev; factor out from npm PR

---
+++ index.js
@@ -0,0 +1,74 @@
+'use strict';
+
+var util = require("util");
+
+function toString(val) {
+    val = val.toString(8);
+    while (val.length < 4) {
+        val = "0" + val;
+    }
+    return val;
+}
+
+var defaultUmask = 18; // 0022;
+var defaultUmaskString = toString(defaultUmask);
+
+function validate(data, k, val) {
+    // must be either an integer or an octal string.
+    if (typeof val === "number" && !isNaN(val)) {
+        data[k] = val;
+        return true;
+    }
+
+    if (typeof val === "string") {
+        if (val.charAt(0) !== "0") {
+            return false;
+        }
+        data[k] = parseInt(val, 8);
+        return true;
+    }
+
+    return false;
+}
+
+function convert_fromString(val, cb) {
+    if (typeof val === "string") {
+        if (!/^[0-7]+$/.test(val)) {
+            return cb(new Error(util.format("Expected octal string, got %j, defaulting to %j",
+                                            val, defaultUmaskString)),
+                      defaultUmask);
+        }
+
+        val = parseInt(val, 8);
+    } else if (typeof val !== "number") {
+        return cb(new Error(util.format("Expected number or octal string, got %j, defaulting to %j",
+                                        val, defaultUmaskString)),
+                  defaultUmask);
+    }
+
+    if ((val < 0) || (val > 511)) {
+        return cb(new Error(util.format("Must be in range 0..511 (0000..0777), got %j", val)),
+                  defaultUmask);
+    }
+
+    cb(null, val);
+}
+
+function fromString(val, cb) {
+
+    // synchronous callback, no zalgo
+    convert_fromString(val, cb || function (err, result) {
+        /*jslint unparam:true*/
+        val = result;
+    });
+
+    return val;
+}
+
+//    if (err) log.warn("invalid umask", err.message)
+
+
+exports.toString = toString;
+exports.fromString = fromString;
+exports.validate = validate;
+
